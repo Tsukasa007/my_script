3ad0dd69cda2fc7c4ab6bca6129b46227a4f188e
YangJiaHui <819582890@qq.com>
2021-07-08 21:26:31 +0800

    合成等待1秒

--- jd_joypark_joy.js
+++ jd_joypark_joy.js
@@ -239,9 +239,8 @@ async function doJoyMergeAll(activityJoyList) {
   let joyBaseInfo = await getJoyBaseInfo()
   let fastBuyLevel = joyBaseInfo.fastBuyLevel
   if (joyMinLevelArr.length >= 2) {
-    $.log(`开始合成 ${minLevel} ${joyMinLevelArr[0].id} <=> ${joyMinLevelArr[1].id}`);
-    $.log(`限流严重，2秒后合成！如失败会重试！`)
-    await $.wait(2000)
+    $.log(`开始合成 ${minLevel} ${joyMinLevelArr[0].id} <=> ${joyMinLevelArr[1].id} 【限流严重，1秒后合成！如失败会重试`);
+    await $.wait(1000)
     await doJoyMerge(joyMinLevelArr[0].id, joyMinLevelArr[1].id);
     await getJoyList()
     await doJoyMergeAll($.activityJoyList)
