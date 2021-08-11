90b7f17eccbc3ef6f921e0b0cf16de06a82063ea
YangJiaHui <819582890@qq.com>
2021-08-11 20:50:42 +0800

    88

--- backup/jd_opencard_teamBean2_enc.js
+++ backup/jd_opencard_teamBean2_enc.js
@@ -22,7 +22,7 @@ cron "44 0,10 * * *" script-path=jd_opencard_teamBean2_enc.js,tag=8.11 组队瓜
 8.11 组队瓜分京豆 = type=cron,script-path=jd_opencard_teamBean2_enc.js, cronexpr="44 0,10 * * *", timeout=3600, enable=true
 */
 const $ = new Env('8.11 组队瓜分京豆');
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
 /*
  * 加密工具已经升级了一个版本，目前为 jsjiami.com.v6 ，更新了加密算法，缩减了体积;
  * 另外 jsjiami.com.v6 已经强制加入校验，注释可以去掉，但是 jsjiami.com.v6 不能去掉，其他都没有任何绑定。
